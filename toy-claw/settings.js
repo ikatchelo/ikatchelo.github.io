1b58e372ceff25a9716c34586179052f5479c174
ikatchelo <ikatchelo@gmail.com>
2025-06-12 01:43:33 +0400

    a

---
+++ toy-claw/settings.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2233769.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 227008116, 227008134, 227008284, 227008213, 227008127, 227008249, 227008095, 227008193, 227008301, 227008198, 227008235, 227008138, 227008282, 227008280, 227008293, 227008133, 227008309, 227008113, 227008085, 227008093, 227008096, 227008147 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'DracoDecoderModule', 'glueUrl' : 'files/assets/227008130/1/draco.wasm.js', 'wasmUrl' : 'files/assets/227008211/1/draco.wasm.wasm', 'fallbackUrl' : 'files/assets/227008129/1/draco.js', 'preload' : true},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/227008307/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/227008117/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/227008109/1/ammo.js', 'preload' : true},
+];
